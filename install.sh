0bf5c0db457c7e9eeb732c2246d547f6c0ee8542
Kevin <kevin@vandenhaute.me>
2023-08-10 10:45:01 +0200

    install AUTOMATIC1111

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+sudo apt-get update && sudo apt-get upgrade
+sudo apt install python3-venv
+
 cd /home/ubuntu
 
-git clone https://github.com/marshmellow77/stable-diffusion-webui.git
+git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
+
+bash stable-diffusion-webui/webui.sh -y
+
+# git clone https://github.com/marshmellow77/stable-diffusion-webui.git
 # bash stable-diffusion-webui/setup.sh -y
