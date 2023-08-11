c3f58a4a687d496c09208031a321cddef7d23adf
Kevin <kevin@vandenhaute.me>
2023-08-11 10:04:51 +0200

    try to get it running again

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # stable-diffusion
 
-ssh -i "~/.ssh/macmini.pem" ubuntu@ec2-54-75-21-30.eu-west-1.compute.amazonaws.com
-
+ssh -i "~/.ssh/macmini.pem" ubuntu@ec2-54-72-2-207.eu-west-1.compute.amazonaws.com
--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get upgrade
-sudo apt install python3-venv libgoogle-perftools4 libtcmalloc-minimal4
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt install python3-venv libgoogle-perftools4 libtcmalloc-minimal4 -y
 
 pip install --upgrade pip
+# pip install torch==2.0.0+cu118 torchmetrics==0.11.4 torchvision==0.15.1+cu118 torchaudio==2.0.1+cu118 torchtext==0.15.1 torchdata==0.6.0 --extra-index-url https://download.pytorch.org/whl/cu118 -U
+# pip install xformers==0.0.19 triton==2.0.0 -U
 
 cd /home/ubuntu
 
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
+chown -R ubuntu:ubuntu stable-diffusion-webui
 
 bash stable-diffusion-webui/webui.sh --share
 
@@ -16,3 +19,7 @@ bash stable-diffusion-webui/webui.sh --share
 
 
 # runuser -l ubuntu
+
+# bash <(wget -qO- https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh)
+
+# python -m pip install lightning
