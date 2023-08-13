6b495e7945bb7d83f97aa28d798f8885dac92771
Kevin <kevin.vandenhaute@gmail.com>
2023-08-13 09:50:26 +0200

    push latest changes

--- install.sh
+++ install.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt install aria2 python3-venv libgoogle-perftools4 libtcmalloc-minimal4 -y
+sudo apt install python3-venv libgoogle-perftools4 libtcmalloc-minimal4 -y
 
 pip install --upgrade pip
 pip install insightface==0.7.3
-# pip install torch==2.0.0+cu118 torchmetrics==0.11.4 torchvision==0.15.1+cu118 torchaudio==2.0.1+cu118 torchtext==0.15.1 torchdata==0.6.0 --extra-index-url https://download.pytorch.org/whl/cu118 -U
-# pip install xformers==0.0.19 triton==2.0.0 -U
 
 cd /home/ubuntu
 
-git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git www
+git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
 
-git clone https://github.com/Gourieff/sd-webui-reactor.git
+bash /home/ubuntu/stable-diffusion/scripts/extensions.sh
+bash /home/ubuntu/stable-diffusion/scripts/embeddings.sh
+bash /home/ubuntu/stable-diffusion/scripts/lora.sh
+bash /home/ubuntu/stable-diffusion/scripts/Stable-diffusion/realisticvision.sh
 
-bash www/webui.sh --share
+cd /home/ubuntu/stable-diffusion-webui
 
-# git clone https://github.com/marshmellow77/stable-diffusion-webui.git
-# bash stable-diffusion-webui/setup.sh -y
-
-
-# runuser -l ubuntu
-
-# bash <(wget -qO- https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh)
-
-# python -m pip install lightning
+bash ./webui.sh --listen
