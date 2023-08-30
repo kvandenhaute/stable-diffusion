5f4a435724eee49b076125fd95955bfe715382bd
Kevin <kevin.vandenhaute@gmail.com>
2023-08-30 20:52:14 +0200

    update

---
+++ resources/sd1.5/checkpoints/analogmadness.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p /home/ubuntu/stable-diffusion-webui/models/Stable-diffusion/
+cd /home/ubuntu/stable-diffusion-webui/models/Stable-diffusion/
+
+wget https://huggingface.co/uf/analogMadness_v5/resolve/main/analogMadness_v50.safetensors
