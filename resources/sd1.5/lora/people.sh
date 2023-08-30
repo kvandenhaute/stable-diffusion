b12be437571e693ac3bbae612c2e8c7de0c2e6b4
Kevin <kevin@vandenhaute.me>
2023-08-30 19:29:54 +0200

    update

--- resources/sd1.5/lora/people.sh
+++ resources/sd1.5/lora/people.sh
@@ -7,4 +7,4 @@ wget -O Breasts_Helper_Trail_v2.safetensors https://civitai.com/api/download/mod
 # medium breast, pink nipples, normal_nipples, normal areola
 
 wget -O ConniePussy_v01.safetensors https://civitai.com/api/download/models/25264
-# <lora:ConniePussy_v01:0.8> between 0.5 and 0.8 for best results
+# <lora:ConniePussy_v01:0.6> between 0.5 and 0.8 for best results
